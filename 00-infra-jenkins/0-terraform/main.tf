24d18dbc6adfb74a55862ae0e1ff515cba18439b
rafael <rafael.mamede90@gmail.com>
2021-11-23 04:16:32 +0000

    primeira parte infra jenkins

---
+++ 00-infra-jenkins/0-terraform/main.tf
@@ -0,0 +1,76 @@
+provider "aws" {
+  region = "sa-east-1"
+}
+
+
+resource "aws_instance" "jenkins" {
+  subnet_id                   = "subnet-06efd40357f45eadf"
+  ami                         = "ami-0e66f5495b4efdd0f"
+  instance_type               = "t2.large"
+  associate_public_ip_address = true
+  key_name                    = "itau-treinamento"
+  tags = {
+    Name = "projeto-jenkins"
+  }
+  vpc_security_group_ids = ["${aws_security_group.jenkins.id}"]
+}
+
+resource "aws_security_group" "jenkins" {
+  name        = "acessos_jenkins"
+  description = "acessos_jenkins inbound traffic"
+  vpc_id      = "vpc-08eb8eaf23779e64b"
+  ingress = [
+    {
+      description      = "SSH from VPC"
+      from_port        = 22
+      to_port          = 22
+      protocol         = "tcp"
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = ["::/0"]
+      prefix_list_ids  = null,
+      security_groups : null,
+      self : null
+    },
+    {
+      description      = "SSH from VPC"
+      from_port        = 8080
+      to_port          = 8080
+      protocol         = "tcp"
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = ["::/0"]
+      prefix_list_ids  = null,
+      security_groups : null,
+      self : null
+    },
+  ]
+
+  egress = [
+    {
+      from_port        = 0
+      to_port          = 0
+      protocol         = "-1"
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = ["::/0"],
+      prefix_list_ids  = null,
+      security_groups : null,
+      self : null,
+      description : "Libera dados da rede interna"
+    }
+  ]
+
+  tags = {
+    Name = "projeto-jenkins-secgrp"
+  }
+}
+
+# terraform refresh para mostrar o ssh
+output "jenkins" {
+  value = [
+    "jenkins",
+    "id: ${aws_instance.jenkins.id}",
+    "private: ${aws_instance.jenkins.private_ip}",
+    "public: ${aws_instance.jenkins.public_ip}",
+    "public_dns: ${aws_instance.jenkins.public_dns}",
+    "ssh -i ~/aws/key01 ubuntu@${aws_instance.jenkins.public_dns}"
+  ]
+}
