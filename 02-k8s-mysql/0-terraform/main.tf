8d945161b7444d762dd0c3883a846c7457c19b84
mauriciogcosta <mauriciogcosta@uol.com.br>
2021-11-24 18:17:02 +0000

    criando MySql dev, stage e prod

---
+++ 02-k8s-mysql/0-terraform/main.tf
@@ -0,0 +1,123 @@
+provider "aws" {
+  region = "sa-east-1"
+}
+
+data "http" "myip" {
+  url = "http://ipv4.icanhazip.com" # outra opção "https://ifconfig.me"
+}
+
+resource "aws_instance" "mysql-dev" {
+  ami           = "ami-0e66f5495b4efdd0f"
+  instance_type = "t2.medium"
+  key_name      = "treinamentoitau_mauricio2"
+  subnet_id = "subnet-06445727840016fb0"
+  root_block_device {
+    encrypted = true
+    volume_size = 8
+  }
+  
+  tags = {
+    Name = "mysql-dev-projeto-final"
+  }
+  vpc_security_group_ids = [aws_security_group.acesso_sql_server.id]
+}
+
+
+resource "aws_instance" "mysql-stage" {
+  ami           = "ami-0e66f5495b4efdd0f"
+  instance_type = "t2.medium"
+  key_name      = "treinamentoitau_mauricio2"
+  subnet_id = "subnet-06445727840016fb0"
+  root_block_device {
+    encrypted = true
+    volume_size = 8
+  }
+  
+  tags = {
+    Name = "mysql-stage-projeto-final"
+  }
+  vpc_security_group_ids = [aws_security_group.acesso_sql_server.id]
+}
+
+resource "aws_instance" "mysql-prod" {
+  ami           = "ami-0e66f5495b4efdd0f"
+  instance_type = "t2.medium"
+  key_name      = "treinamentoitau_mauricio2"
+  subnet_id = "subnet-06445727840016fb0"
+  root_block_device {
+    encrypted = true
+    volume_size = 8
+  }
+  
+  tags = {
+    Name = "mysql-prod-projeto-final"
+  }
+  vpc_security_group_ids = [aws_security_group.acesso_sql_server.id]
+}
+
+resource "aws_security_group" "acesso_sql_server" {
+  name        = "acesso_sql_server"
+  description = "Habilita porta padrao mysql"
+  vpc_id = "vpc-006b099589ba3289e"
+  ingress = [
+    {
+      description      = "Porta padrao acesso_sql_server"
+      from_port        = 1433
+      to_port          = 1433
+      protocol         = "tcp"
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = ["::/0"]
+      prefix_list_ids = null,
+      security_groups: null,
+      self: null
+    },
+    {
+      description      = "SSH from VPC"
+      from_port        = 22
+      to_port          = 22
+      protocol         = "tcp"
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = []
+      prefix_list_ids = null,
+      security_groups: null,
+      self: null
+    },
+  ]
+egress = [
+    {
+      from_port        = 0
+      to_port          = 0
+      protocol         = "-1"
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = [],
+      prefix_list_ids = null,
+      security_groups: null,
+      self: null,
+      description: "Libera dados da rede interna"
+    }
+  ]
+
+  tags = {
+    Name = "acesso_mysql_server"
+  }
+}
+
+output "mysql" {
+  value = [
+    aws_instance.mysql-dev.private_ip, 
+    aws_instance.mysql-stage.private_ip,
+    aws_instance.mysql-prod.private_ip,
+    
+  ]
+}
+
+output "mysql-dev-projeto-final" {
+  value = [
+       "mysql-dev - ${aws_instance.mysql-dev.private_ip} - ssh -i ~/.ssh/treinamentoitau_mauricio2.pem ubuntu@${aws_instance.mysql-dev.private_ip} -o ServerAliveInterval=60",
+       "mysql-stage - ${aws_instance.mysql-stage.private_ip} - ssh -i ~/.ssh/treinamentoitau_mauricio2.pem ubuntu@${aws_instance.mysql-stage.private_ip} -o ServerAliveInterval=60",
+       "mysql-prod - ${aws_instance.mysql-prod.private_ip} - ssh -i ~/.ssh/treinamentoitau_mauricio2.pem ubuntu@${aws_instance.mysql-prod.private_ip} -o ServerAliveInterval=60"
+  ]
+
+}
+
+# terraform refresh para mostrar o ssh
---
+++ 02-k8s-mysql/deploy.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+# ID_M1_DNS="ubuntu@ec2-18-234-97-123.compute-1.amazonaws.com"
+# ID_M1_DNS=$(echo "$ID_M1_DNS" | cut -b 8-)
+# echo ${ID_M1_DNS}
+
+#### idéia para buscar itens do debugger do ansible ####
+# | grep -oP "(kubeadm join.*?certificate-key.*?)'" | sed 's/\\//g' | sed "s/'//g" | sed "s/'t//g" | sed "s/,//g"
+
+cd 02-k8s-mysql/0-terraform
+terraform init
+terraform apply -auto-approve
+
+echo  "Aguardando a criação das maquinas ..."
+sleep 5
+
+ID_M1=$(terraform output | grep 'mysql-dev -' | awk '{print $4;exit}')
+ID_M1_DNS=$(terraform output | grep 'mysql-dev -' | awk '{print $8;exit}' | cut -b 8-)
+
+ID_M2=$(terraform output | grep 'mysql-stage -' | awk '{print $4;exit}')
+ID_M2_DNS=$(terraform output | grep 'mysql-stage -' | awk '{print $8;exit}' | cut -b 8-)
+
+ID_M3=$(terraform output | grep 'mysql-prod -' | awk '{print $4;exit}')
+ID_M3_DNS=$(terraform output | grep 'mysql-prod -' | awk '{print $8;exit}' | cut -b 8-)
+
+
+# ID_HAPROXY=$(terraform output | grep 'k8s_proxy -' | awk '{print $3;exit}')
+# ID_HAPROXY_DNS=$(terraform output | grep 'k8s_proxy -' | awk '{print $8;exit}' | cut -b 8-)
+
+
+# ID_W1=$(terraform output | grep 'k8s-workers 1 -' | awk '{print $4;exit}')
+# ID_W1_DNS=$(terraform output | grep 'k8s-workers 1 -' | awk '{print $9;exit}' | cut -b 8-)
+
+# ID_W2=$(terraform output | grep 'k8s-workers 2 -' | awk '{print $4;exit}')
+# ID_W2_DNS=$(terraform output | grep 'k8s-workers 2 -' | awk '{print $9;exit}' | cut -b 8-)
+
+# ID_W3=$(terraform output | grep 'k8s-workers 3 -' | awk '{print $4;exit}')
+# ID_W3_DNS=$(terraform output | grep 'k8s-workers 3 -' | awk '{print $9;exit}' | cut -b 8-)
+
+echo "
+[ec2-mysql-dev]
+$ID_M1_DNS
+[ec2-mysql-stage]
+$ID_M2_DNS
+[ec2-mysql-prod]
+$ID_M3_DNS
+
+
+" > ..02-k8s-mysql/1-ansible/hosts
+
+ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i hosts provisionar.yml -u ubuntu --private-key /var/lib/jenkins/treinamentoitau_mauricio2.pem
