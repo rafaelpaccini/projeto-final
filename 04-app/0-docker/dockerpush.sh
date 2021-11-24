5d334edb4100e5374cef0b3e2b3203b9ac9d54fa
mauriciogcosta <mauriciogcosta@uol.com.br>
2021-11-24 20:34:16 +0000

    build docker e docker push

---
+++ 04-app/0-docker/dockerpush.sh
@@ -0,0 +1,3 @@
+tag=$(git describe --tags $(git rev-list --tags --max-count=1))
+docker tag projetofinal/spring-web-youtube:$tag hub.docker.com/r/projetofinal/spring-web-youtube:$tag
+docker push projetofinal/spring-web-youtube:$tag
