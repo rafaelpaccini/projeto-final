05baa90f36d3ab6ab4f86213cc191ab2380b309d
mauriciogcosta <mauriciogcosta@uol.com.br>
2021-11-25 02:22:11 +0000

    teste mamede

---
+++ mamede-teste/deploy.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+ANSIBLE_OUT=$(ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i hosts provisionar.yml -u ubuntu --private-key /var/lib/jenkins/treinamentoitau_mauricio2.pem)
+echo $ANSIBLE_OUT
+
+## Mac ##
+# MYSQL_POD_NAME=$(echo $ANSIBLE_OUT | grep -oE "(mysql-.*? )" )
+## Linux ##
+# MYSQL_POD_NAME=$(echo $ANSIBLE_OUT | grep -oP "(mysql-.*? )" )
+
+echo "Esperando subir os pods ..."
+sleep 300
+
+cat <<EOF > restore-dump-mysql.yml
+- hosts: 
+  - mysql-dev
+  become: yes
+  tasks:
+    - name: "Upload dump"
+      copy:
+        src: "k8s-deploy/1.2-dump-mysql.sql"
+        dest: "/root/"
+
+    - name: "Create dabatase"
+      shell: echo "create database SpringWebYoutubeTest;" 
+    
+    - name: "Restore dump mysql"
+      shell: "mysql root -p'root' < /root/1.2-dump-mysql.sql"
+EOF
+
+#ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i hosts restore-dump-mysql.yml -u ubuntu --private-key /var/lib/jenkins/treinamentoitau_mauricio2.pem
