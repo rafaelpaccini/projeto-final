efd331aa3a33d2e6cfd47b9db0022f5ce8223877
mauriciogcosta <mauriciogcosta@uol.com.br>
2021-11-25 02:44:45 +0000

    acerto push docker

--- 04-app/0-docker/dockerpush.sh
+++ 04-app/0-docker/dockerpush.sh
@@ -1,3 +1,4 @@
+cd spring-web-youtube
 tag=$(git describe --tags $(git rev-list --tags --max-count=1))
 docker tag projetofinal/spring-web-youtube:$tag hub.docker.com/r/projetofinal/spring-web-youtube:$tag
 docker push projetofinal/spring-web-youtube:$tag
