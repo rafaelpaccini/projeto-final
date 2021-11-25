e451fb47307c73f3e35ba244d819148650f45c53
mauriciogcosta <mauriciogcosta@uol.com.br>
2021-11-25 02:41:34 +0000

    docker tag

--- 04-app/0-docker/build-docker.sh
+++ 04-app/0-docker/build-docker.sh
@@ -5,6 +5,7 @@
 # docker build -t "didox/crud-java-login:$tag" -f Dockerfile .
 
 #### Pegando ultima tag do git #####
+cd spring-web-youtube
 tag=$(git describe --tags $(git rev-list --tags --max-count=1))
 echo "Fazendo deploy da tag $tag"
 docker build -t "projetofinal/spring-web-youtube:$tag" -f 04-app/0-docker/Dockerfile .
