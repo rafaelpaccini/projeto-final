f110443e8d72ccc52ee19cddc3dfdd1b7b285eb0
mauriciogcosta <mauriciogcosta@uol.com.br>
2021-11-25 02:42:50 +0000

    acerto docher build

--- 04-app/0-docker/build-docker.sh
+++ 04-app/0-docker/build-docker.sh
@@ -8,4 +8,5 @@
 cd spring-web-youtube
 tag=$(git describe --tags $(git rev-list --tags --max-count=1))
 echo "Fazendo deploy da tag $tag"
+cd ..
 docker build -t "projetofinal/spring-web-youtube:$tag" -f 04-app/0-docker/Dockerfile .
